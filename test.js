95f69b0bbf4c3bce9cc2f6461b7c555b8d1dec53
rifat-craftsmen <rifat.ahmed@craftsmensoftware.com>
2026-02-06 11:53:12 +0600

    x

--- test.js
+++ test.js
@@ -1,9 +1,3 @@
-async function sum(a, b) {
+function sum(a, b) {
     let c = a+b;
-<<<<<<< HEAD
-    console.log(a);
-    console.log(b);
-=======
->>>>>>> 8bd58f93d82f0e51746f42618f76ba8d5bada096
-    console.log(c);
 }
